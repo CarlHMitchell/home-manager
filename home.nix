797bde408c6264799af6262ef63b88931c2d57c4
Carl Mitchell <carl.mitchell@gomotive.com>
2025-07-30 20:23:29 -0400

    fix: move vscode to own section

--- home.nix
+++ home.nix
@@ -51,34 +51,6 @@
     jj-fzf
     btop
     jetbrains.clion
-    vscode-fhs
-    vscode-extensions.bbenoist.nix
-    vscode-extensions.ms-vscode.cpptools # Useful, but eats disk space
-    vscode-extensions.yzhang.markdown-all-in-one
-    vscode-extensions.ms-python.python
-    vscode-extensions.timonwong.shellcheck
-    vscode-extensions.twxs.cmake
-    vscode-extensions.ms-vscode.cmake-tools
-    vscode-extensions.rust-lang.rust-analyzer
-    vscode-extensions.mechatroner.rainbow-csv
-    vscode-extensions.ms-azuretools.vscode-docker
-    vscode-extensions.xaver.clang-format
-    vscode-extensions.arrterian.nix-env-selector
-    vscode-extensions.mkhl.direnv
-    vscode-extensions.zxh404.vscode-proto3
-    vscode-extensions.eamodio.gitlens
-    vscode-extensions.ms-toolsai.jupyter
-    vscode-extensions.ms-toolsai.vscode-jupyter-slideshow
-    vscode-extensions.ms-toolsai.vscode-jupyter-cell-tags
-    vscode-extensions.ms-toolsai.jupyter-renderers
-    vscode-extensions.ms-toolsai.jupyter-keymap
-    vscode-extensions.jnoortheen.nix-ide
-    vscode-extensions.ms-vscode.makefile-tools
-    vscode-extensions.davidanson.vscode-markdownlint
-    vscode-extensions.jebbs.plantuml
-    vscode-extensions.shd101wyy.markdown-preview-enhanced
-    vscode-extensions.github.copilot
-    vscode-extensions.visualjj.visualjj
     slack
     dia
     gimp
@@ -229,6 +201,43 @@
   };
 
   programs.ssh.addKeysToAgent = "yes";
+
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscode-fhs;
+    mutableExtensionsDir = true;
+    profiles.default = {
+      enableExtensionUpdateCheck = true;
+      extensions = with pkgs.vscode-extensions; [
+        bbenoist.nix
+        ms-vscode.cpptools # Useful, but eats disk space
+        yzhang.markdown-all-in-one
+        ms-python.python
+        timonwong.shellcheck
+        twxs.cmake
+        ms-vscode.cmake-tools
+        rust-lang.rust-analyzer
+        mechatroner.rainbow-csv
+        ms-azuretools.vscode-docker
+        xaver.clang-format
+        arrterian.nix-env-selector
+        mkhl.direnv
+        zxh404.vscode-proto3
+        eamodio.gitlens
+        ms-toolsai.jupyter
+        ms-toolsai.vscode-jupyter-slideshow
+        ms-toolsai.vscode-jupyter-cell-tags
+        ms-toolsai.jupyter-renderers
+        ms-toolsai.jupyter-keymap
+        jnoortheen.nix-ide
+        ms-vscode.makefile-tools
+        davidanson.vscode-markdownlint
+        jebbs.plantuml
+        shd101wyy.markdown-preview-enhanced
+        visualjj.visualjj
+      ];
+    };
+  };
 
   # Nicely reload services when changing configs
   systemd.user.startServices = "sd-switch";
