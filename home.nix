07c88c8c0871499574ffc3c939050463f90d64ed
Carl Mitchell <carl.mitchell@gomotive.com>
2025-10-13 11:41:38 -0400

    fix: edit jj signing, try to ignore KTMR LFS files

--- home.nix
+++ home.nix
@@ -240,11 +240,15 @@
       user = {
         email = "carl.mitchell@gomotive.com";
         name = "Carl Mitchell";
-        signing.sign-all = true;
+        signing.behavior = "own";
         signing.backend = "ssh";
         signing.key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ6uxGJ1DLnFasXcRQYp7tM7UL0vVfV+5Fg7IKSxGfuu carl@carl-thinkpad-pw0j0jnb";
+        git = {
+          sign-on-push = true;
+        };
         ui = {
           dif-formatter = ["difft" "--color=always" "$left" "$right"];
+          show-cryptographic-signatures = true; # May be slow
         };
       };
     };
