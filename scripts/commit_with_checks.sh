26e53797fd406c03aa61be716d60210e4c831b01
Carl Mitchell <carl.mitchell@gomotive.com>
2026-02-18 15:59:00 -0500

    feat: add prek to new com alias script

--- scripts/commit_with_checks.sh
+++ scripts/commit_with_checks.sh
@@ -2,5 +2,14 @@
 
 set -eEuo pipefail
 
+WORKING_DIR="$(pwd)"
+TEMP_FILE="$(mktemp)"
+printf "%s" "${WORKING_DIR}">"${TEMP_FILE}"
+if rg "\/home\/carl\/code\/KeepTruckin\/kt" "${TEMP_FILE}" >/dev/null; then
+  uvx --with pre-commit jj-pre-push --checker "/home/carl/.config/home-manager/scripts/prek_ktmr.sh" check
+else
+  uvx --with pre-commit jj-pre-push --checker prek check
+fi
+
 /home/carl/.config/home-manager/scripts/conventional_commit_check.sh "$@"
 jj new
