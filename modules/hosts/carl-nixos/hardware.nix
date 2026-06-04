eedd5977d93e6cf70f613cc46a573cd5cab88764
Carl Mitchell <github@eufalconimorph.com>
2026-06-04 17:22:44 -0400

    fix: add 32-bit graphics drivers for Steam

--- modules/hosts/carl-nixos/hardware.nix
+++ modules/hosts/carl-nixos/hardware.nix
@@ -13,6 +13,13 @@
     boot.extraModulePackages = [];
 
     nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-    hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+    hardware = {
+      cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+      graphics = {
+        enable = true;
+        enable32Bit = true; # needed for Steam
+      };
+    };
   };
 }
